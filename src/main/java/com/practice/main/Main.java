32ff704e0899c996e3cc2eab4316f65f2218f8f8
amrshaw4695 <amrita.sharma.connect@gmail.com>
2025-06-05 22:19:07 +0530

    some feature v1 commit 1

--- src/main/java/com/practice/main/Main.java
+++ src/main/java/com/practice/main/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!!");
         System.out.println("Amrita");
+        System.out.println("feature some-feature-v1");
     }
 }
